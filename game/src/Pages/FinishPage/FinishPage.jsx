338f19580c5387e1eb1ee2afea4c5146d6fbc941
RLis <lisss17@mail.ru>
2023-11-11 21:05:15 +0500

    add new bg

--- game/src/Pages/FinishPage/FinishPage.jsx
+++ game/src/Pages/FinishPage/FinishPage.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "../FinishPage/FinishPage.css";
+import Game from "../Game/Game";
 
 const FinishPage = (props) => {
+
+  const onClickHandler = () => {
+    return <Game></Game>
+
+  }
 
   return (
     <div className="container-finish">
       <div className="scores">Твои очки: {props.score}</div>
-      <button onClick={props.deleteResult} className="restart_btn">
+      <button onClick={onClickHandler} className="restart_btn">
         Начать заново
       </button>
     </div>
