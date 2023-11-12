046f99ed05ebcbf9fc006d96139d01a622ebe44d
KatiaProkina <71662309+KatiaProkina@users.noreply.github.com>
2023-11-12 12:37:53 +0300

    fix username input

--- game/src/Pages/FinishPage/FinishPage.jsx
+++ game/src/Pages/FinishPage/FinishPage.jsx
@@ -1,24 +1,23 @@
 import { useState } from "react";
 import "./FinishPage.css";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios'
+import axios from "axios";
 
 const FinishPage = ({ score }) => {
   const navigate = useNavigate();
   // Состояние для имени пользователя
-  const [username, setUsername] = useState(""); 
+  const [username, setUsername] = useState("");
 
   const onClickHandler = () => {
     // Перейти обратно на главную
     navigate("/");
   };
 
-    // Отправить данные на сервер или выполнить другие необходимые действия
+  // Отправить данные на сервер или выполнить другие необходимые действия
   const sendResultHandler = () => {
     sendResultsToServer(username, score);
-  }
-
-
+    setUsername("");
+  };
 
   const sendResultsToServer = async (username, score) => {
     try {
@@ -26,12 +25,15 @@ const FinishPage = ({ score }) => {
       if (!score || score === 0) {
         score = 0;
       }
-  
-      const response = await axios.post("https://localhost:44365/api/players/", {
-        username: username,
-        score: score,
-      });
-  
+
+      const response = await axios.post(
+        "https://localhost:44365/api/players/",
+        {
+          username: username,
+          score: score,
+        }
+      );
+
       // Выводим данные из ответа в консоль
       console.log("Данные успешно отправлены на сервер:", response.data);
     } catch (error) {
@@ -44,25 +46,27 @@ const FinishPage = ({ score }) => {
     <div className="container-finish">
       <div className="scores">Твои очки: {score}</div>
       <button type="button" onClick={onClickHandler} className="restart_btn">
-          Начать заново
-        </button>
+        Начать заново
+      </button>
       {/* Добавляем форму для ввода имени пользователя */}
 
       <div className="form_wrapper">
-      <form className="form">
-        <label>
-          Имя пользователя:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        
-      </form>
+        <form className="form">
+          <label>
+            Имя пользователя:
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+        </form>
 
-
-        <button type="button" onClick={sendResultHandler} className="server_btn">
+        <button
+          type="button"
+          onClick={sendResultHandler}
+          className="server_btn"
+        >
           Отправить результат
         </button>
       </div>
@@ -71,8 +75,3 @@ const FinishPage = ({ score }) => {
 };
 
 export default FinishPage;
-
-
-
-
-
